c4966d866af53b44139a6044c7b3bc91739c22d6
Jerlin Wong <jerlin.wong@cognizant.com>
2023-10-08 23:32:58 +0800

    add spec to return 1 when string is 1

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,9 +4,14 @@ require './app/string_calculator'
 
 RSpec.describe StringCalculator do
   describe '#add' do
-  subject { StringCalculator.new }
+    subject { StringCalculator.new }
+
     it 'when string is empty return 0' do
-      expect(subject.add("")).to eq(0)
+      expect(subject.add('')).to eq(0)
+    end
+
+    it 'when string is "1", return 1' do
+      expect(subject.add('1')).to eq(1)
     end
   end
 end
