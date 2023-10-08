88fafbf6c3dc6974b4570741db8e0fc5f977a301
Jerlin Wong <jerlin.wong@cognizant.com>
2023-10-08 23:47:38 +0800

    return addition of numbers when 1 or 2 numbers are given as input

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -4,6 +4,12 @@ class StringCalculator
   def add(input_string)
     if input_string.empty?
       0
+    elsif
+      output = 0
+      input_string.split(",").each do |num|
+        output = num.to_i + output
+      end
+      output
     end
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,11 +7,15 @@ RSpec.describe StringCalculator do
     subject { StringCalculator.new }
 
     it 'when string is empty return 0' do
-      expect(subject.add('')).to eq(0)
+      expect(subject.add("")).to eq(0)
     end
 
     it 'when string is "1", return 1' do
-      expect(subject.add('1')).to eq(1)
+      expect(subject.add("1")).to eq(1)
+    end
+
+    it 'when string is "1,2", return 3' do
+      expect(subject.add("1,2")).to eq(3)
     end
   end
 end
